b9e347c8fd42ed79d0e8b98d0c9f9bcc94fd87b2
Kenny Pflug <kenny.pflug@live.de>
2021-03-08 21:21:13 +0100

    added header bar

---
+++ components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header(): JSX.Element {
+    return (
+        <header>
+            Hello
+        </header>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { Header } from 'components';
 import '../styles/globals.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <Header />
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
 
